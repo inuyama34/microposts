93fcbce443e090bd51710189984b95df66e36861
hiroyuki.inuyama <kaigaigame34@gmail.com>
2019-10-20 07:36:03 +0000

    kansei2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   
- before_action :require_user_logged_in, only: [:index, :show, :followings, :followers,]
+ before_action :require_user_logged_in, only: [:index, :show, :followings, :followers,:likes]
 
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
